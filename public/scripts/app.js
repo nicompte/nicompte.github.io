316d0a8f090ebe65c87975ecb3a7da29ed3f5ac7
Nicolas Barbotte <n.barbotte@gmail.com>
2014-05-15 14:36:34 +0200

    améliorations

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,6 +16,26 @@
 ga('create', 'UA-41369793-1', 'barbotte.net');
 ga('send', 'pageview');
 
+
+$(function(){
+
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        || location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+  
+})
+
 // Angular app init
 var app = angular.module('barbotte', ['ngRoute']);
 
